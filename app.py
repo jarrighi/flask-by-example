3789592b26d97130dc48595da361eb6c18c7141d
Jules Arrighi <juliana.arrighi@gmail.com>
2014-11-16 13:52:00 -0800

    Add index template and handle POSTs

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-  return "Hello World!"
+  errors = []
+  results = {}
+  if request.method == "POST":
+    try: 
+      url = request.form['url']
+      print url[:7]
+      if "http://" not in url[:7]:
+        url = 'http://' + url
+      r = requests.get(url)
+      print r.text
+    except:
+      errors.append(
+        "Unable to get URL. Please make sure it's valid and try again."
+      )
+
+  return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
