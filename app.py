4dc00001983a5091743e215f9c23d7eff47fdb67
Jules Arrighi <juliana.arrighi@gmail.com>
2014-11-18 00:47:35 -0800

    Implement Redis Queue to do the counting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
@@ -8,70 +8,90 @@ import os
 import requests
 import re
 import nltk
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 # Configuration
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
+q = Queue(connection=conn)
 
 from models import Result
+
+# Helpers
+def count_and_save_words(url):
+  errors = []
+
+  try:
+    r = requests.get(url)
+  except:
+    errors.append(
+      "Unable to get URL. Please make sure it's valid and try again."
+    )
+    return {"error": errors}
+
+  raw = BeautifulSoup(r.text).get_text()
+  nltk.data.path.append('./nltk_data/')
+  tokens = nltk.word_tokenize(raw)
+  text = nltk.Text(tokens)
+
+  nonPunct = re.compile('.*[A-Za-z].*')
+  raw_words = [w for w in text if nonPunct.match(w)]
+  raw_word_count = Counter(raw_words)
+
+  no_stop_words = [w for w in raw_words if w.lower() not in stops]
+  no_stop_words_count = Counter(no_stop_words)
+  
+  try:
+    result = Result(
+      url=url,
+      result_all=raw_word_count,
+      result_no_stop_words=no_stop_words_count
+    )
+    db.session.add(result)
+    db.session.commit()
+    return result.id
+  except:
+    errors.append("Unable to add item to database.")
+    return {"error": errors}
 
 # Routes
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-  errors = []
   results = {}
-  r = ''
   if request.method == "POST":
-    try: 
-      url = request.form['url']
-      print url[:7]
-      if "http" not in url[:7]:
-        url = 'http://' + url
-      r = requests.get(url)
-      print "r:" + r.text
-    except:
-      errors.append(
-        "Unable to get URL. Please make sure it's valid and try again."
-      )
+    url = request.form['url']
+    if "http" not in url[:4]:
+      url = 'http://' + url
+  
+    job = q.enqueue_call(func=count_and_save_words, args=(url,), result_ttl=5000)
+    print job.get_id()
 
-    if r:
-      raw = BeautifulSoup(r.text).get_text()
-      nltk.data.path.append('./nltk_data/')
-      tokens = nltk.word_tokenize(raw)
-      text = nltk.Text(tokens)
-
-      nonPunct = re.compile('.*[A-Za-z].*')
-      raw_words = [w for w in text if nonPunct.match(w)]
-      raw_word_count = Counter(raw_words)
-
-      no_stop_words = [w for w in raw_words if w.lower() not in stops]
-      no_stop_words_count = Counter(no_stop_words)
-
-      results = sorted(
-        no_stop_words_count.items(), 
-        key=operator.itemgetter(1),
-        reverse=True
-      )[:10]
-
-      try:
-        result = Result(
-          url=url,
-          result_all=raw_word_count,
-          result_no_stop_words=no_stop_words_count
-        )
-        db.session.add(result)
-        db.session.commit()
-      except:
-        errors.append("Unable to add item to database.")
-
-  return render_template('index.html', errors=errors, results=results)
+  return render_template('index.html', results=results)
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
+    else:
+        return "Nay!", 202
 
 if __name__ == "__main__":
   app.run(port=5050)
